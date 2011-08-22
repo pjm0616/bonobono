107fdf7f9378a412293549ec25d52d2128e9dd45
siya <devnull@localhost>
2011-08-23 00:00:00 +0900

    motion ui interface for intep

---
+++ js/motion.js
@@ -0,0 +1,22 @@
+function speak(str){}
+function face-left(){}
+function face-right(){}
+
+function face-red(){}
+
+function face-eye(n,sec){}
+function face-mouth(speed,sec){}
+function face-sweat(n){}
+function face-shake(speed,sec){}
+
+function body-left(){}
+function body-right(){}
+
+function body-shell(){}
+function body-shake(speed,deg){}
+function body-arm-shake(speed,deg){}
+function body-walk(speed,sec){}
+
+
+
+
